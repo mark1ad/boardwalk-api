784925d78bce34aadfd222cd7b6b54dc387f71c5
Mark De May <markd@frii.com>
2018-01-31 15:28:55 -0700

    Updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://boardwalk.herokuapp.com'
+    origins 'https://boardwalkfe.herokuapp.com'
 
     resource '*',
       headers: :any,
