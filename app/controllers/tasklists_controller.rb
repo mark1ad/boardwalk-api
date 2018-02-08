842da1dc3f62898915effd1cc1dccb26c4f76000
Mark De May <markd@frii.com>
2018-02-07 17:02:05 -0700

    Tasklists are accessed through /boards/:id/tasklists

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -1,41 +1,54 @@
 class TasklistsController < ApplicationController
   before_action :set_tasklist, only: [:show, :update, :destroy]
 
-  # GET /tasklists
+  # GET boards/1/tasklists
   def index
-    @tasklists = Tasklist.all
+    @tasklists = Tasklist.where(board_id: params[:board_id])
 
     render json: @tasklists
   end
 
-  # GET /tasklists/1
+  # GET boards/1/tasklists/1
   def show
-    render json: @tasklist
-  end
-
-  # POST /tasklists
-  def create
-    @tasklist = Tasklist.new(tasklist_params)
-
-    if @tasklist.save
-      render json: @tasklist, status: :created, location: @tasklist
-    else
-      render json: @tasklist.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /tasklists/1
-  def update
-    if @tasklist.update(tasklist_params)
+    if @tasklist.board_id.to_s() == params[:board_id]
       render json: @tasklist
     else
+      render json: {error: "Not Found"}, status: :not_found
+    end
+  end
+
+  # POST boards/1/tasklists
+  def create
+    @tasklist = Tasklist.new(tasklist_params)
+    @tasklist.board_id = params[:board_id]
+
+    if @tasklist.save
+      render json: @tasklist, status: :created
+    else
       render json: @tasklist.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /tasklists/1
+  # PATCH/PUT boards/1/tasklists/1
+  def update
+    if @tasklist.board_id.to_s() == params[:board_id]
+      if @tasklist.update(tasklist_params)
+        render json: @tasklist
+      else
+        render json: @tasklist.error, status: :unprocessable_entity
+      end
+    else
+      render json: {error: "Not Found"}, status: :not_found
+    end
+  end
+
+  # DELETE boards/1/tasklists/1
   def destroy
-    @tasklist.destroy
+    if @tasklist.board_id.to_s() == params[:board_id]
+      @tasklist.destroy
+    else
+      render json: {error: "Not Found"}, status: :not_found
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :tasklists
-  resources :boards
+  resources :boards do
+    resources :tasklists
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
